1e5f939374f1608feb28a9d5229ea82669463ec9
baraa <baraadanouf@gmail.com>
2022-10-15 16:23:57 -0700

    Initial commit this is just start Flutter

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+   //   home: Splash(),
+    );
+  }
+}
+
